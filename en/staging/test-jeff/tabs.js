f076a8907c4bd1eae10f22f21dad5fc9548ccbf4
Jeff Doktor <jdoktor@brightcove.com>
2019-06-27 10:39:12 -0600

    test tabs

--- en/staging/test-jeff/tabs.js
+++ en/staging/test-jeff/tabs.js
@@ -17,42 +17,43 @@ jQuery(document).ready(function () {
   	  // NOTE: doesn't take into account existing params
   		history.replaceState("", "", currentAttrValue);
   	}
-
-    var queryString = {};
-  	window.location.href.replace(
-  	    new RegExp("([^?=&]+)(=([^&]*))?", "g"),
-  	    function($0, $1, $2, $3) { queryString[$1] = $3; }
-  	);
-    // if (queryString[base.options.param]) {
-    //
-    // 	var tab = $("a[href='#" + queryString[base.pathname] + "']");
-    //
-    // 	tab
-    // 		.closest(".nav")
-    // 		.find("a")
-    // 		.removeClass("active")
-    // 		.end()
-    // 		.next(".list-wrap")
-    // 		.find("ul")
-    // 		.hide();
-    // 	tab.addClass("active");
-    // 	$("#" + queryString[base.pathname]).show();
-    //
-    // }
-
-    // var h = window.location.hash,
-    // tabLinks = document.querySelectorAll('.tabs .tab-links a');
-    //
-    // if (h.length > 0) {
-    //   var iMax = tabLinks.length,
-    //     i;
-    //   for (i = 0; i < iMax; i++) {
-    //     if (tabLinks[i].getAttribute('href') === h) {
-    //       tabLinks[i].click();
-    //     }
-    //   }
-    // }
   });
+
+  var h = window.location.hash,
+  tabLinks = document.querySelectorAll('.tabs .tab-links a');
+
+  if (h.length > 0) {
+    var iMax = tabLinks.length,
+      i;
+    for (i = 0; i < iMax; i++) {
+      if (tabLinks[i].getAttribute('href') === h) {
+        tabLinks[i].click();
+      }
+    }
+  }
+
+  // console.log("href: ", window.location.href);
+  // var tab = window.location.href.split("#")[1];
+  // console.log("tab: ", tab);
+
+  // var queryString = {};
+  // window.location.href.replace(
+  //     new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+  //     function($0, $1, $2, $3) { queryString[$1] = $3; }
+  // );
+  // if (tab) {
+  // 	tab
+  // 		.closest(".nav")
+  // 		.find("a")
+  // 		.removeClass("active")
+  // 		.end()
+  // 		.next(".list-wrap")
+  // 		.find("ul")
+  // 		.hide();
+  // 	tab.addClass("active");
+  // 	$("#" + queryString[base.pathname]).show();
+  //
+  // }
 
 
 });

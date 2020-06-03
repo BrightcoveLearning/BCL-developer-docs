968773983e55b102c2da10c3ef340e70ace3511d
Jeff Doktor <jdoktor@brightcove.com>
2020-06-03 08:59:49 -0600

    ios release script

--- scripts/ios-sdk-plugins.sh
+++ scripts/ios-sdk-plugins.sh
@@ -1,162 +1,189 @@
 #!/bin/bash
 # This script copies the latest iOS SDK and Plugin files from GitHub and replaces what resides on BCL-developer-docs
-echo "Copy iOS SDK and Plugin GitHub Repositories"
+echo " "
+echo "This is a script to clone the latest iOS SDK and plugin GitHub Repositories."
+echo "Then, the html repo contents are copied into BCL-developer-docs."
+echo " "
+# To run this script, open Terminal app for the scripts directory.
+# bash ios-sdk-plugins.sh
 
-# Enter the path to your local GitHub repo for BCL-developer-docs
-mypath=~/Downloads/
-echo $mypath
-
-#**** iOS SDK
-# If the input iOS SDK directory exists, then delete it
-if [ -d "brightcove-player-sdk-ios" ]; then
-    echo "input iOS SDK Directory found and removed"
-    rm -rf "brightcove-player-sdk-ios"
-fi
-
-# If the input iOS SDK directory doesn't exist, then clone a copy of it
-if [ ! -d "brightcove-player-sdk-ios" ]; then
-  echo "iOS SDK directory NOT found, cloning Repo"
-    git clone https://github.com/brightcove/brightcove-player-sdk-ios.git
-fi
-
-# If the output iOS SDK directory exists, delete it
-sdkpath=$mypath"ios-sdk"
-echo "output dir = " $sdkpath
-if [ -d $sdkpath ]; then
-    echo "deleting iOS SDK in BCL-developer-docs"
-    rm -rf $sdkpath
-fi
-
-# Copy iOS SDK html folder to BCL-developer-docs
-cp -a brightcove-player-sdk-ios/html/. $sdkpath
-
-
-#**** FreeWheel plugin
-# If the input FreeWheel plugin directory exists, then delete it
-if [ -d "brightcove-player-sdk-ios-fw" ]; then
-    echo "FreeWheel Plugin Directory found and removed"
-    rm -rf "brightcove-player-sdk-ios-fw"
-fi
-
-# Clone the FreeWheel plugin
-git clone https://github.com/brightcove/brightcove-player-sdk-ios-fw.git
-
-# If the output FreeWheel plugin directory exists, delete it
-fwpath=$mypath"ios-plugins/freewheel"
-echo "output dir = " $fwpath
-if [ -d $fwpath ]; then
-    echo "deleting FreeWheel plugin in BCL-developer-docs"
-    rm -rf $fwpath
-fi
-
-# Copy FreeWheel html folder to BCL-developer-docs
-cp -a brightcove-player-sdk-ios-fw/html/. $fwpath
-
-
-#**** Googlecast plugin
-# If the input Googlecast plugin directory exists, then delete it
-if [ -d "brightcove-player-sdk-ios-googlecast" ]; then
-    echo "Googlecast Plugin Directory found and removed"
-    rm -rf "brightcove-player-sdk-ios-googlecast"
-fi
-
-# Clone the Googlecast plugin
-git clone https://github.com/brightcove/brightcove-player-sdk-ios-googlecast.git
-
-# If the output Googlecast plugin directory exists, delete it
-gcpath=$mypath"ios-plugins/googlecast"
-echo "output dir = " $gcpath
-if [ -d $gcpath ]; then
-    echo "deleting Googlecast plugin in BCL-developer-docs"
-    rm -rf $gcpath
-fi
-
-# Copy Googlecast html folder to BCL-developer-docs
-cp -a brightcove-player-sdk-ios-googlecast/html/. $gcpath
+# #**** iOS SDK
+# # If the input iOS SDK directory exists, then delete it
+# if [ -d "data/brightcove-player-sdk-ios" ]; then
+#     echo "input iOS SDK Directory found and removed"
+#     rm -rf "data/brightcove-player-sdk-ios"
+# fi
+#
+# # If the input iOS SDK directory doesn't exist, then clone a copy of it
+# if [ ! -d "data/brightcove-player-sdk-ios" ]; then
+#   echo "iOS SDK directory NOT found, cloning Repo"
+#     git clone https://github.com/brightcove/brightcove-player-sdk-ios.git
+# fi
+#
+# # If the output iOS SDK directory exists, delete it
+# sdkpath=$mypath"ios-sdk"
+# echo "output dir = " $sdkpath
+# if [ -d $sdkpath ]; then
+#     echo "deleting iOS SDK in BCL-developer-docs"
+#     rm -rf $sdkpath
+# fi
+#
+# # Copy iOS SDK html folder to BCL-developer-docs
+# cp -a brightcove-player-sdk-ios/html/. $sdkpath
+#
+#
+# #**** FreeWheel plugin
+# # If the input FreeWheel plugin repo exists, then delete it
+# echo " "
+# if [ -d "ios-repos/brightcove-player-sdk-ios-fw" ]; then
+#     rm -rf "ios-repos/brightcove-player-sdk-ios-fw"
+#     echo "FreeWheel plugin repo deleted"
+# fi
+#
+# # # Clone the FreeWheel plugin repo
+# cd ios-repos
+# git clone https://github.com/brightcove/brightcove-player-sdk-ios-fw.git
+# echo "FreeWheel plugin repo cloned"
+# cd ..
+#
+# # If the output FreeWheel plugin doc exists, delete it
+# fwpath="ios-docs/freewheel"
+# if [ -d $fwpath ]; then
+#     echo "Deleting FreeWheel docs folder: " $PWD/$fwpath
+#     rm -rf $fwpath
+# fi
+#
+# # Copy FreeWheel html folder to doc
+# cp -a ios-repos/brightcove-player-sdk-ios-fw/html/. $fwpath
+# echo "FreeWheel html folder copied into docs folder: " $PWD/$fwpath
 
 
-#**** IMA plugin
-# If the input IMA plugin directory exists, then delete it
-if [ -d "brightcove-player-sdk-ios-ima" ]; then
-    echo "IMA Plugin Directory found and removed"
-    rm -rf "brightcove-player-sdk-ios-ima"
-fi
-
-# Clone the IMA plugin
-git clone https://github.com/brightcove/brightcove-player-sdk-ios-ima.git
-
-# If the output IMA plugin directory exists, delete it
-imapath=$mypath"ios-plugins/ima"
-echo "output dir = " $imapath
-if [ -d $imapath ]; then
-    echo "deleting IMA plugin in BCL-developer-docs"
-    rm -rf $imapath
-fi
-
-# Copy IMA html folder to BCL-developer-docs
-cp -a brightcove-player-sdk-ios-ima/html/. $imapath
-
-
-#**** Omniture plugin
-# If the input Omniture plugin directory exists, then delete it
-if [ -d "brightcove-player-sdk-ios-omniture" ]; then
-    echo "Omniture Plugin Directory found and removed"
-    rm -rf "brightcove-player-sdk-ios-omniture"
-fi
-
-# Clone the Omniture plugin
-git clone https://github.com/brightcove/brightcove-player-sdk-ios-omniture.git
-
-# If the output Omniture plugin directory exists, delete it
-ompath=$mypath"ios-plugins/omniture"
-echo "output dir = " $ompath
-if [ -d $ompath ]; then
-    echo "deleting Omniture plugin in BCL-developer-docs"
-    rm -rf $ompath
-fi
-
-# Copy Omniture html folder to BCL-developer-docs
-cp -a brightcove-player-sdk-ios-omniture/html/. $ompath
+# #**** Googlecast plugin
+# # If the input Googlecast plugin repo exists, then delete it
+# echo " "
+# if [ -d "ios-repos/brightcove-player-sdk-ios-googlecast" ]; then
+#     rm -rf "ios-repos/brightcove-player-sdk-ios-googlecast"
+#     echo "Googlecast plugin repo deleted"
+# fi
+#
+# # Clone the Googlecast plugin
+# cd ios-repos
+# git clone https://github.com/brightcove/brightcove-player-sdk-ios-googlecast.git
+# echo "Googlecast plugin repo cloned"
+# cd ..
+#
+# # If the output Googlecast plugin doc exists, delete it
+# gcpath="ios-docs/googlecast"
+# if [ -d $gcpath ]; then
+#     echo "Deleting Googlecast docs folder" $PWD/$gcpath
+#     rm -rf $gcpath
+# fi
+#
+# # Copy Googlecast html folder to doc
+# cp -a ios-repos/brightcove-player-sdk-ios-googlecast/html/. $gcpath
+# echo "Googlecast html folder copied into docs folder: " $PWD/$gcpath
 
 
-#**** Pulse plugin
-# If the input Pulse plugin directory exists, then delete it
-if [ -d "brightcove-player-sdk-ios-pulse" ]; then
-    echo "Pulse Plugin Directory found and removed"
-    rm -rf "brightcove-player-sdk-ios-pulse"
-fi
+# #**** IMA plugin
+# # If the input IMA plugin repo exists, then delete it
+# echo " "
+# if [ -d "ios-repos/brightcove-player-sdk-ios-ima" ]; then
+#     rm -rf "ios-repos/brightcove-player-sdk-ios-ima"
+#     echo "IMA plugin repo deleted"
+# fi
+#
+# # Clone the IMA plugin
+# cd ios-repos
+# git clone https://github.com/brightcove/brightcove-player-sdk-ios-ima.git
+# echo "IMA plugin repo cloned"
+# cd ..
+#
+# # If the output IMA plugin doc exists, delete it
+# imapath="ios-docs/ima"
+# if [ -d $imapath ]; then
+#     echo "Deleting IMA plugin docs folder" $PWD/$imapath
+#     rm -rf $imapath
+# fi
+#
+# # Copy IMA html folder to doc
+# cp -a ios-repos/brightcove-player-sdk-ios-ima/html/. $imapath
+# echo "IMA html folder copied into docs folder: " $PWD/$imapath
 
-# Clone the Pulse plugin
-git clone https://github.com/brightcove/brightcove-player-sdk-ios-pulse.git
 
-# If the output Pulse plugin directory exists, delete it
-pulsepath=$mypath"ios-plugins/pulse"
-echo "output dir = " $pulsepath
-if [ -d $pulsepath ]; then
-    echo "deleting Pulse plugin in BCL-developer-docs"
-    rm -rf $pulsepath
-fi
+# #**** Omniture plugin
+# # If the input Omniture plugin repo exists, then delete it
+# echo " "
+# if [ -d "ios-repos/brightcove-player-sdk-ios-omniture" ]; then
+#     rm -rf "ios-repos/brightcove-player-sdk-ios-omniture"
+#     echo "Omniture plugin repo deleted"
+# fi
+#
+# # Clone the Omniture plugin
+# cd ios-repos
+# git clone https://github.com/brightcove/brightcove-player-sdk-ios-omniture.git
+# cd ..
+#
+# # If the output Omniture plugin doc exists, delete it
+# ompath="ios-docs/omniture"
+# if [ -d $ompath ]; then
+#     echo "Deleting Omniture plugin in docs folder" $PWD/$ompath
+#     rm -rf $ompath
+# fi
+#
+# # Copy Omniture html folder to doc
+# cp -a ios-repos/brightcove-player-sdk-ios-omniture/html/. $ompath
+# echo "Omniture html folder copied into docs folder: " $PWD/$ompath
 
-# Copy Pulse html folder to BCL-developer-docs
-cp -a brightcove-player-sdk-ios-pulse/html/. $pulsepath
 
-#**** SSAI plugin
-# If the input SSAI plugin directory exists, then delete it
-if [ -d "brightcove-player-sdk-ios-ssai" ]; then
-    echo "SSAI Plugin Directory found and removed"
-    rm -rf "brightcove-player-sdk-ios-ssai"
-fi
+# #**** Pulse plugin
+# # If the input Pulse plugin repo exists, then delete it
+# echo " "
+# if [ -d "ios-repos/brightcove-player-sdk-ios-pulse" ]; then
+#     rm -rf "ios-repos/brightcove-player-sdk-ios-pulse"
+#     echo "Pulse plugin repo deleted"
+# fi
+#
+# # Clone the Pulse plugin
+# cd ios-repos
+# git clone https://github.com/brightcove/brightcove-player-sdk-ios-pulse.git
+# cd ..
+#
+# # If the output Pulse plugin doc exists, delete it
+# pulsepath="ios-docs/pulse"
+# if [ -d $pulsepath ]; then
+#     echo "Deleting Pulse plugin in docs folder"
+#     rm -rf $pulsepath
+# fi
+#
+# # Copy Pulse html folder to doc
+# cp -a ios-repos/brightcove-player-sdk-ios-pulse/html/. $pulsepath
+# echo "Pulse html folder copied into docs folder: " $PWD/$pulsepath
+#
+#
+# #**** SSAI plugin
+# # If the input SSAI plugin repo exists, then delete it
+# echo " "
+# if [ -d "ios-repos/brightcove-player-sdk-ios-ssai" ]; then
+#     rm -rf "ios-repos/brightcove-player-sdk-ios-ssai"
+#     echo "SSAI plugin repo deleted"
+# fi
 
 # Clone the SSAI plugin
+cd ios-repos
 git clone https://github.com/brightcove/brightcove-player-sdk-ios-ssai.git
+cd ..
 
-# If the output SSAI plugin directory exists, delete it
-ssaipath=$mypath"ios-plugins/ssai"
-echo "output dir = " $ssaipath
+# If the output SSAI plugin doc exists, delete it
+ssaipath="ios-docs/ssai"
 if [ -d $ssaipath ]; then
-    echo "deleting SSAI plugin in BCL-developer-docs"
+    echo "Deleting SSAI plugin in docs folder"
     rm -rf $ssaipath
 fi
 
 # Copy SSAI html folder to BCL-developer-docs
-cp -a brightcove-player-sdk-ios-ssai/html/. $ssaipath
+cp -a ios-repos/brightcove-player-sdk-ios-ssai/html/. $ssaipath
+echo "SSAI html folder copied into docs folder: " $PWD/$ssaipath
+
+echo " "
+echo "Congratulations! The iOS SDK and plugin document files have been successfully copied from GitHub."
+echo "If this is a test, discard all changes in GitHub Desktop. Otherwise, commit changes."
